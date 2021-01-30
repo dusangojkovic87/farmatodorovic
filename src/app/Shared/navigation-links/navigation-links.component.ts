d7013e29be9c971fedf1f040f72fba5d79d73a16
Dusan Gojkovic <dusangojkovic087gmail.com>
2021-01-30 03:54:04 -0800

    uklonjen slash iz css sa assets

--- src/app/Shared/navigation-links/navigation-links.component.ts
+++ src/app/Shared/navigation-links/navigation-links.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationLinksComponent implements OnInit {
  navState:boolean = false;
- bars:string = "/assets/icons/bars.svg";
- close:string = "/assets/icons/close.svg"
+ bars:string = "assets/icons/bars.svg";
+ close:string = "assets/icons/close.svg"
 
   constructor() { }
 
